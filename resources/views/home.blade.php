fbd0d9e039fe551ac488c003049b71c38029a649
Pievisco89 <pie-89@live.it>
2021-06-23 15:39:20 +0200

    Finito con bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 
-    <ul>
+    <div class="container d-flex flex-wrap justify-content-center">
+
         @foreach ($movies as $movie)
-            <li> {{ $movie['title'] }} </li>
+            <div class="card border-success m-4">
+                <div class="card-header">Titolo: <strong>{{$movie['title']}}</strong> </div>
+                <div class="card-body">
+                <h5 class="card-title">Titolo originale: <strong>{{$movie['original_title']}}</strong> </h5>
+                <h6 class="card-text">Data di uscita: <strong>{{$movie['date']}}</strong> </h6>
+                <h6 class="card-text">Voto: {{$movie['vote']}}</h6>
+                </div>
+            </div>
+            
         @endforeach
-    </ul>
+
+    </div>
+
 
 @endsection
